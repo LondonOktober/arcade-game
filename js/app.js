f6c44b5441a6bc2ee30e66d7f44169cf9f17475b
London Oktober <london@walkingtoast.com>
2018-10-29 09:35:09 -0700

    WIP2
    Work in Progress 2 update

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 const Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.speed = Math.round(Math.random() * 3) + 1;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -13,16 +13,28 @@ const Enemy = function(x, y) {
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    this.x = (this.x + (dt*200));
+    this.x = (this.x + this.speed + dt * 10) % 500;
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    // Checks for collision between player and enemies
+    if (player.x < this.x + 60 &&
+        player.x + 60 > this.x &&
+        player.y < this.y + 40 &&
+        40 + player.y > this.y) {
+        player.x = 202;
+        player.y = 375;
+    };
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+
+Enemy.prototype.reset = function() {
+    this.speed = Math.round(Math.random() * 3) + 1;
+}
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -33,7 +45,7 @@ const Player = function() {
   this.y = 375;
 }
 
-Player.prototype.update = function() {
+Player.prototype.update = function(dt) {
 
 };
 
@@ -43,13 +55,13 @@ Player.prototype.render = function() {
 
 Player.prototype.handleInput = function(key) {
   if (key) {
-    if (key == 'left') {
+    if (key == 'left' && this.x > 0) {
       this.x -= 101;
-    } else if (key == 'right') {
+    } else if (key == 'right' && this.x < 375) {
       this.x += 101;
-    } else if (key == 'up') {
+    } else if (key == 'up' && this.y > 0) {
       this.y -= 86;
-    } else if (key == 'down') {
+    } else if (key == 'down' && this.y < 375) {
       this.y += 86;
     }
   }
@@ -63,7 +75,7 @@ let enemy2 = new Enemy(-100, 142);
 let enemy3 = new Enemy(-100, 224);
 let allEnemies = [enemy1, enemy2, enemy3];
 
-let player = new Player();
+let player = new Player(202, 375);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
