580be37f49f5b49377e24aa49ecbb3e9f6b22235
London Oktober <london@walkingtoast.com>
2018-10-28 17:35:21 -0700

    WIP
    Work in Progress

--- js/app.js
+++ js/app.js
@@ -1,16 +1,19 @@
 // Enemies our player must avoid
-var Enemy = function() {
+const Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
+    this.x = (this.x + (dt*200));
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -24,18 +27,48 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+const Player = function() {
+  this.sprite = 'images/char-cat-girl.png';
+  this.x = 202;
+  this.y = 375;
+}
 
+Player.prototype.update = function() {
+
+};
+
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(key) {
+  if (key) {
+    if (key == 'left') {
+      this.x -= 101;
+    } else if (key == 'right') {
+      this.x += 101;
+    } else if (key == 'up') {
+      this.y -= 86;
+    } else if (key == 'down') {
+      this.y += 86;
+    }
+  }
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+let enemy1 = new Enemy(-100, 58);
+let enemy2 = new Enemy(-100, 142);
+let enemy3 = new Enemy(-100, 224);
+let allEnemies = [enemy1, enemy2, enemy3];
 
-
+let player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    const allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
